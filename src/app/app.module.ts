ab39736a38dfe1dfab938546b34ffdabc0bf1bf0
gaspromov <gaspromov@mail.ru>
2020-09-02 15:02:40 +0300

    верстка главной страницы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main/main.component';
+import { HeaderComponent } from './main/header/header.component';
+import { NavbarComponent } from './main/navbar/navbar.component';
+import { MonitorsComponent } from './main/monitors/monitors.component';
+import { SuccessesComponent } from './main/successes/successes.component';
+import { SocialComponent } from './main/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    NavbarComponent,
+    MonitorsComponent,
+    SuccessesComponent,
+    SocialComponent,
   ],
   imports: [
     BrowserModule,
